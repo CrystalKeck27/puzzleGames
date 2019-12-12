f437ead32cb7c06069431bb4754501bde3d06205
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2019-12-11 22:31:44 -0600

    added hiDPI canvas support

--- games/signposts/script.js
+++ games/signposts/script.js
@@ -1,5 +1,4 @@
-var cnv = document.getElementById('gameCanvas');
-var ctx = cnv.getContext('2d');
-setCanvas(cnv);
+setCanvasFromId('gameCanvas');
 rect(0,0,100,100);
-ctx.fillRect(100,100,100,100);
+gfx.ctx.font = '30px Arial';
+gfx.ctx.fillText('Hello World', 50, 150);
--- libraries/graphics.js
+++ libraries/graphics.js
@@ -2,20 +2,37 @@ var gfx = {
     //the reference to the canvas
     cnv: null,
     //the reference to the context
-    ctx: null
+    ctx: null,
+    //the base transform (needed for hiDPI transformations)
+    transform: null
 }
 
-function setCanvas(canvas){
+function setCanvas(canvas, hiDPI = true){
     gfx.cnv = canvas;
     updateContext();
+    if(hiDPI) makeHiDPI();
+}
+
+function makeHiDPI(){
+    if (window.devicePixelRatio > 1) {
+        var canvasWidth = gfx.cnv.width;
+        var canvasHeight = gfx.cnv.height;
+    
+        gfx.cnv.width = canvasWidth * window.devicePixelRatio;
+        gfx.cnv.height = canvasHeight * window.devicePixelRatio;
+        gfx.cnv.style.width = canvasWidth;
+        gfx.cnv.style.height = canvasHeight;
+    
+        gfx.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    }
 }
 
 function updateContext(){
     gfx.ctx = gfx.cnv.getContext('2d');
 }
 
-function setCanvasFromId(id){
-    setCanvas(document.getElementById(id));
+function setCanvasFromId(id, hiDPI = true){
+    setCanvas(document.getElementById(id), hiDPI);
 }
 
 function rect(x, y, width, height){
