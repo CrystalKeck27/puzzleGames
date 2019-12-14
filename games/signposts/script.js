0f8740d7a362f805d12d19e9111c118bd1d67fa8
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2019-12-13 22:58:25 -0600

    major upgrade to gameLoop library

--- games/signposts/script.js
+++ games/signposts/script.js
@@ -1,11 +1,25 @@
 setCanvasFromId('gameCanvas');
+
+var x = 0;
+var b = 0;
+
 function draw(delta) {
     stroke(255);
     fill(255);
     rect(0, 0, 400, 400);
     stroke(0);
     fill(0);
-    text(1000/delta, 100, 100);
+    text(x, 100, 100);
 }
 
-initializeDeltaGameLoop(draw);
+function every(a){
+    x=a-b;
+    b=a;
+    if(a>10000){
+        x = 42069;
+        stopLoop();
+    }
+}
+
+schedule(every, 30);
+initializeGameLoop(draw);
--- libraries/gameLoop.js
+++ libraries/gameLoop.js
@@ -1,40 +1,81 @@
-var _gameLoop = {
+var _gl = {
     running: false,
     stopToken: null,
-    previousFrame: 0,
+    lastRender: 0,
     events: []
 }
 
+    // depreciated DO NOT USE
     function initializeBasicGameLoop(callback) {
         (function() {
             function main(tFrame) {
-                _gameLoop.running = true;
+                _gl.running = true;
                 callback(tFrame);
-                _gameLoop.stopToken = window.requestAnimationFrame(main);
+                _gl.stopToken = window.requestAnimationFrame(main);
             }
             main(0);
         })();
     }
 
+    // depreciated DO NOT USE
     function initializeDeltaGameLoop(callback) {
         (function() {
             function main(tFrame) {
-                _gameLoop.running = true;
-                var delta = tFrame - _gameLoop.previousFrame;
-                _gameLoop.previousFrame = tFrame;
+                _gl.running = true;
+                var delta = tFrame - _gl.lastRender;
+                _gl.lastRender = tFrame;
                 callback(delta);
-                _gameLoop.stopToken = window.requestAnimationFrame(main);
+                _gl.stopToken = window.requestAnimationFrame(main);
             }
             main(0);
         })();
-
     }
 
     function stopLoop() {
-        window.cancelAnimationFrame(_gameLoop.stopToken);
-        _gameLoop.running = false;
+        window.cancelAnimationFrame(_gl.stopToken);
+        _gl.running = false;
     }
-    
-    function schedule(callback, cps){
-        
+
+    function schedule(callback, cps) {
+        _gl.events.push({
+            fn: callback,
+            rate: cps,
+            len: 1000 / cps,
+            last: 0,
+            calls: 0
+        })
+    }
+
+    function initializeGameLoop(renderCallback) {
+        (function() {
+            function main(tframe) {
+                _gl.running = true;
+                var delta = tframe - _gl.lastRender;
+                _gl.lastRender = tframe;
+                _gl.events.forEach(function(event) {
+                    //calculate the next call timestamp
+                    var nextTick = event.last + event.len;
+                    //declare numTicks with default
+                    var numTicks = 0;
+                    //init numTicks
+                    //x=tframe;
+                    if (tframe > nextTick) {
+                        numTicks = Math.floor((tframe - nextTick) / event.len);
+                    }
+                    //call the event numTicks times and update last in th process
+                    for (var i = 0; i < numTicks; i++) {
+                        event.last += event.len;
+                        event.fn(event.last);
+                    }
+                });
+                renderCallback(delta, tframe);
+                _gl.stopToken = window.requestAnimationFrame(main);
+            }
+            
+            _gl.lastRender = performance.now();
+            _gl.events.forEach(function(event) {
+                event.last = _gl.lastRender;
+            });
+            main(_gl.lastRender);
+        })();
     }
