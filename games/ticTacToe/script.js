e7f83c7f634c9decb8d0039a7c4ab96220968e11
MichaelKolaski123 <58784496+MichaelKolaski123@users.noreply.github.com>
2020-01-06 08:34:06 -0600

    found the location of the mouse when the user clicks and set up an if statement that i will later use to put the X's and O's in the box that was clicked

--- games/ticTacToe/script.js
+++ games/ticTacToe/script.js
@@ -3,3 +3,38 @@ rect(150, 0, 5, 450);
 rect(300, 0, 5, 450);
 rect(0, 150, 450, 5);
 rect(0, 300, 450, 5);
+var x, y;
+gfx.cnv.onclick = function(e){
+    x = e.offsetX;
+    y = e.offsetY;
+    if (x < 150) {
+        if (y < 150) {
+            //top left
+        } else if (y > 150 && y < 300) {
+            //middle left
+        } else if (y < 300) {
+            //bottom left
+        }
+    } else if (x > 150 && x < 300) {
+        if (y < 150) {
+            //top middle
+        } else if (y > 150 && y < 300) {
+            //middle middle
+        } else if (y < 300) {
+            //bottom middle
+        }
+    } else if (x > 300) {
+        if (y < 150) {
+            //top right
+        } else if (y > 150 && y < 300) {
+            //middle right
+        } else if (y < 300) {
+            //bottom right
+        }
+    }
+}
+/* function draw(){
+    ellipse(x, y, 100, 100);
+}
+
+initializeGameLoop(draw); */
