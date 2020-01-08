b49166a4e8782e9eb7fc35a6addd2b76d8ef0e29
MichaelKolaski123 <58784496+MichaelKolaski123@users.noreply.github.com>
2020-01-07 21:40:03 -0600

    Changed all the code to make it shorter. Tic tac toe works as intended. All there is left is to add a winner detection system.

--- games/ticTacToe/script.js
+++ games/ticTacToe/script.js
@@ -1,97 +1,58 @@
 setCanvasFromId('gameCanvas');
-strokeWeight(5);
-line(150, 0, 150, 450);
-line(300, 0, 300, 450);
-line(0, 150, 450, 150);
-line(0, 300, 450, 300);
-var x, y;
-var whatToDraw = 0
-gfx.cnv.onclick = function(e) {
-    x = e.offsetX;
-    y = e.offsetY;
-    console.log(x, y);
-    if (x < 150) {
-        if (y < 150) {
-            //top left
-            if (whatToDraw === 0) {
-                drawAnX(75, 75);
-            } else {
-                drawAnO(75, 75);
-            }
-        } else if (y > 150 && y < 300) {
-            //middle left
-            if (whatToDraw === 0) {
-                drawAnX(75, 225);
-            } else {
-                drawAnO(75, 225);
-            }
-        } else if (y > 300) {
-            //bottom left
-            if (whatToDraw === 0) {
-                drawAnX(75, 375);
-            } else {
-                drawAnO(75, 375);
-            }
-        }
-} else if (x > 150 && x < 300) {
-        if (y < 150) {
-            //top middle
-            if (whatToDraw === 0) {
-                drawAnX(225, 75);
-            } else {
-                drawAnO(225, 75);
-            }
-        } else if (y > 150 && y < 300) {
-            //middle middle
-            if (whatToDraw === 0) {
-                drawAnX(225, 225);
-            } else {
-                drawAnO(225, 225);
-            }
-        } else if (y > 300) {
-            //bottom middle
-            if (whatToDraw === 0) {
-                drawAnX(225, 375);
-            } else {
-                drawAnO(225, 375);
-            }
-        }
-    } else if (x > 300) {
-        if (y < 150) {
-            //top right
-            if (whatToDraw === 0) {
-                drawAnX(375, 75);
-            } else {
-                drawAnO(375, 75);
-            }
-        } else if (y > 150 && y < 300) {
-            //middle right
-            if (whatToDraw === 0) {
-                drawAnX(375, 225);
-            } else {
-                drawAnO(375, 225);
-            }
-        } else if (y > 300) {
-            //bottom right
-            if (whatToDraw === 0) {
-                drawAnX(375, 375);
-            } else {
-                drawAnO(375, 375);
-            }
-        }
-    }
-    if (whatToDraw === 0) {
-        whatToDraw = 1
-    } else {
-        whatToDraw = 0
+var isXTurn = true;
+var data = [];
+for (let i = 0; i < 3; i++) {
+    data[i] = [];
+    for (let j = 0; j < 3; j++) {
+        data[i][j] = 0;
     }
 }
 
+function render() {
+    background(255);
+    strokeWeight(5);
+    noFill();
+    line(150, 0, 150, 450);
+    line(300, 0, 300, 450);
+    line(0, 150, 450, 150);
+    line(0, 300, 450, 300);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let px = j * 150 + 75;
+            let py = i * 150 + 75;
+            if (data[i][j] == 1) {
+                drawAnX(px, py);
+            } else if (data[i][j] == 2) {
+                drawAnO(px, py);
+            }
+        }
+    }
+}
+
+gfx.cnv.onclick = function(e) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+    x /= 150;
+    y /= 150;
+    x = Math.floor(x);
+    y = Math.floor(y);
+    if (data[y][x] === 0) {
+        if (isXTurn) {
+            data[y][x] = 1;
+        } else {
+            data[y][x] = 2;
+        }
+        isXTurn = !isXTurn;
+    }
+};
+
 function drawAnO(x, y) {
-    ellipse(x, y, 65, 65);
+    ellipse(x, y, 65);
 }
 
 function drawAnX(x, y) {
     line(x + 65, y + 65, x - 65, y - 65);
     line(x - 65, y + 65, x + 65, y - 65);
 }
+
+initializeGameLoop(render);
