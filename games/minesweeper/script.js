536f487f57cded44322cf055d65359b2b4980134
MichaelKolaski123 <58784496+MichaelKolaski123@users.noreply.github.com>
2020-01-11 21:06:42 -0600

    getting minesweeper files started

--- games/minesweeper/script.js
+++ games/minesweeper/script.js
@@ -1,1 +1,16 @@
-//eat me arse
+setCanvasFromId("gameCanvas")
+//class definitions
+class cell {
+    constructor()
+}
+//variable definitions
+
+var rows = 8,
+    cols = 8;
+var grid = []
+for (let y = 0; y < rows; y++) {
+    grid[y] = []
+    for (letx = 0; x < cols; x++) {
+        grid[y][x] = 0
+    }
+}
