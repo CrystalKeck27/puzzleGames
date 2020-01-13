b4a55242ddf6a7ac141def53cebce627da7de9b7
MichaelKolaski123 <58784496+MichaelKolaski123@users.noreply.github.com>
2020-01-12 22:00:17 -0600

    continued work on minesweeper

--- games/minesweeper/script.js
+++ games/minesweeper/script.js
@@ -1,16 +1,39 @@
-setCanvasFromId("gameCanvas")
+setCanvasFromId("gameCanvas");
 //class definitions
 class cell {
-    constructor()
+    constructor(isMine, isCovered, isFlagged, adjacentMines) {
+        this.isMine = isMine;
+        this.isCovered = isCovered;
+        this.isFlagged = isFlagged;
+        this.adjacentMines = adjacentMines;
+    }
 }
 //variable definitions
+var rows, cols, numMines;
+var grid = [];
 
-var rows = 8,
-    cols = 8;
-var grid = []
-for (let y = 0; y < rows; y++) {
-    grid[y] = []
-    for (letx = 0; x < cols; x++) {
-        grid[y][x] = 0
+reset(8, 8, 10);
+
+//util functions
+function reset(rowsParam, colsParam, numMinesParam) {
+    rows = rowsParam;
+    cols = colsParam;
+    numMines = numMinesParam;
+    for (let y = 0; y < rows; y++) {
+        grid[y] = [];
+        for (let x = 0; x < cols; x++) {
+            grid[y][x] = new cell();
+        }
+    }
+}
+
+function generate() {
+    if (numMines > rows * cols) throw RangeError("Too many mines");
+    let temp = [];
+    for (let i = 0; i < rows; y++) {
+        temp.push(...grid[y]);
+    }
+    for (let i = 0; i < numMines; i++) {
+
     }
 }
