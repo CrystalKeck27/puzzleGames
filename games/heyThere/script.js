73adbbe48e832246ddeb25b3f1aa76eda4ed529a
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2020-01-13 11:41:49 -0600

    more work on heyThere

--- games/heyThere/script.js
+++ games/heyThere/script.js
@@ -1,6 +1,48 @@
+//Some spacial startup things to allow fullscreen
 setCanvasFromId("gameCanvas");
+document.onclick = function () {
+    gfx.cnv.requestFullscreen({ navigationUI: "hide" }).then(setTimeout(setup, 250));
+}
+gfx.cnv.width = 0;
+gfx.cnv.height = 0;
 
-gfx.cnv.width = screen.width;
-gfx.cnv.height = screen.height;
+document.onfullscreenchange = function () {
+    gfx.cnv.width = 0;
+    gfx.cnv.height = 0;
+    document.getElementById("msgBox").innerHTML = "Game Paused";
+    document.onclick = function () {
+        gfx.cnv.requestFullscreen({ navigationUI: "hide" }).then(setTimeout(setup, 250));
+    }
+}
 
-requestCanvasFullscreen();
+function setupScreen() {
+    document.onclick = null;
+    gfx.cnv.width = screen.width;
+    gfx.cnv.height = screen.height;
+    gfx.cnv.focus();
+    setup();
+}
+
+//Variables
+var hasStarted = false;
+
+//Functions
+function setup() {
+    if (hasStarted) {
+        console.log("resume");
+        initializeGameLoop(render);
+    } else {
+        console.log("startup");
+        initializeGameLoop(render);
+        hasStarted = true;
+    }
+}
+
+function stop() {
+    stopLoop();
+}
+
+function render() {
+    background(255);
+
+}
