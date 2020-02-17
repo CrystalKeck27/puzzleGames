e4fe62dc0642caac4ec0c6062f79b2971d278af9
MichaelKolaski123 <58784496+MichaelKolaski123@users.noreply.github.com>
2020-02-16 17:16:19 -0800

    added comments before the lines explaining what they do

--- libraries/graphicsClass.js
+++ libraries/graphicsClass.js
@@ -12,13 +12,21 @@ class ExtendedCanvas {
             this.ctx = this.cnv.getContext("2d");
             //throw an error if reference to the context was null
             if (!this.ctx) throw new Error("Unable to get Context");
+            //check if the reference is a canvas
         } else if (reference.toString() == "[object HTMLCanvasElement]") {
+            //set this.cnv to the reference
             this.cnv = reference;
+            //set this.ctx to the context
             this.ctx = this.cnv.getContext("2d");
+            //throw an error if reference to the context was null
             if (!this.ctx) throw new Error("Unable to get Context");
+            //check if the reference is a context
         } else if (reference.toString() == "[object CanvasRenderingContext2D") {
+            //set this.ctx to the reference
             this.ctx = reference;
+            //set this.cnv to the pointer to the canvas
             this.cnv = this.ctx.canvas;
+            //throw an error if the canvas was null
             if(!this.cnv) throw new Error("Unable to get Canvas");
         }
     }
